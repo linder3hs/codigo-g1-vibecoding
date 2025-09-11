164c77ab26b7973ece13150f68c086726b2133e4
linder3hs <linderhassinger00@gmail.com>
2025-09-10 22:47:47 -0500

    coderabbit

---
+++ todo-vibecoding/test.ts
@@ -0,0 +1,11 @@
+// create a some function to test TS
+function testTS() {
+  const a: number = 1;
+  const b: number = 2;
+  const c: number = a + b;
+  return c;
+}
+
+// test the function
+const result = testTS();
+console.log(result);
